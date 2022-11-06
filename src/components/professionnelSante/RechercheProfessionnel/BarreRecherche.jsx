c32d987480ea96a6a4407e4ad3048fda7f07e0fd
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 17:05:22 +0300

    Message by stp

--- src/components/professionnelSante/RechercheProfessionnel/BarreRecherche.jsx
+++ src/components/professionnelSante/RechercheProfessionnel/BarreRecherche.jsx
@@ -1,7 +1,16 @@
+import "./BarreRecherche.css"
+
 function BarreRecherche() {
   return (
-    <div className="App">
-    Barre de recherche
+    <div className="container">
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
+            <span className="fa fa-search"></span>
+          </div>
+        </div>
+        <input type="text" className="form-control" name="" value="">
+      </div>
     </div>
   );
 }
