a51fbda67fa715c39cf3bafb883677bb42f84f73
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 21:24:58 +0300

    Message by stp

--- src/components/professionnelSante/RechercheProfessionnel/LaboratoireContainer.jsx
+++ src/components/professionnelSante/RechercheProfessionnel/LaboratoireContainer.jsx
@@ -1,4 +1,6 @@
 import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+
 
 function LaboratoireContainer() {
   return (
@@ -6,7 +8,17 @@ function LaboratoireContainer() {
         <Box className="shadow w-90">
           <div className="row">
             <div className="col-sm-6">
-            Details laboratoire here
+              <div className="row">
+              <div className="col-sm-6">
+              Logo laboratoire here
+              </div>
+              <div className="col-sm-6">
+              info laboratoire here
+              </div>
+              <div className="col-sm-12">
+              <Button variant="contained">Prendre rendez-vous</Button>
+              </div>
+              </div>
             </div>
             <div className="col-sm-6">
             Details Disponibilité here
