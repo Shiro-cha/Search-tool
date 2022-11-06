1605ba37f6d0257f9d543e60affcefc4771697f1
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 18:06:28 +0300

    Message by stp

--- src/components/professionnelSante/RechercheProfessionnel/BarreRecherche.jsx
+++ src/components/professionnelSante/RechercheProfessionnel/BarreRecherche.jsx
@@ -6,12 +6,12 @@ function BarreRecherche() {
   return (
     <div className="container">
       <div className="input-group">
-        <div className="input-group-prepend">
+        <div className="input-group-prepend bg-none">
           <div className="input-group-text">
-            <Search />
+            <Search className="btn-Search"/>
           </div>
         </div>
-        <input type="text" className="form-control" name="" value="" />
+        <input type="text" className="form-control" name="" value="" placeholder="Nom, spécialité, établissement..."/>
         <div className="input-group-prepend">
           <Button>Recherce</Button>
         </div>
