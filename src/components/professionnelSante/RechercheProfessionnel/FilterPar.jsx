9956c208275bf6fa63a29ffb88d70f8921937e6d
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 19:43:41 +0300

    Message by stp

--- src/components/professionnelSante/RechercheProfessionnel/FilterPar.jsx
+++ src/components/professionnelSante/RechercheProfessionnel/FilterPar.jsx
@@ -1,8 +1,10 @@
+import Typography from "@mui/material/Typography"
+
 function FilterPar() {
   return (
     <div className="container-fluid">
       <div className="navbar navbar-expand bg-light mt-2">
-        <h4>Filtrer par</h4>
+        <Typography>Filtrer par</Typography>
         <div className="navbar-nav">
         <div className="nav-item">
         Disponibilité
