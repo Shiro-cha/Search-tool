487581d932ea83b06fd40ebefde5d4c314a8563d
Shiro-cha <noum.rzdr@gmail.com>
2022-11-06 19:41:29 +0300

    Message by stp

--- src/components/professionnelSante/RechercheProfessionnel/FilterPar.jsx
+++ src/components/professionnelSante/RechercheProfessionnel/FilterPar.jsx
@@ -1,7 +1,7 @@
 function FilterPar() {
   return (
     <div className="container-fluid">
-      <div className="navbar navbar-expand bg-light">
+      <div className="navbar navbar-expand bg-light mt-2">
         <h4>Filtrer par</h4>
         <div className="navbar-nav">
         <div className="nav-item">
